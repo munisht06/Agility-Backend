fd5df50bfd579acb0b3c5b987dba65add067be35
munisht06 <munishtanwar77@gmail.com>
2022-06-11 23:51:15 -0400

    static page added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
-const {errorHandler} = require("./middleware/errorMiddleware");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const meetings = require("./routes/meetingRoutes");
 const users = require("./routes/userRoutes");
 
@@ -13,10 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//serve index.html for "/"
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 app.use("/api/meetings", meetings);
 app.use("/api/users", users);
 
 app.use(errorHandler);
-
 
 module.exports = app;
