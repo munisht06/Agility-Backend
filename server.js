3a872a0cbd017b9519b25ced24a7a14c4af4a860
munisht06 <munishtanwar77@gmail.com>
2022-06-13 14:52:13 -0400

    fix

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const app = require("./index");
-const PORT = process.env.PORT || 8000;
+// const app = require("./index");
+// const PORT = process.env.PORT || 8000;
 
-app.listen(8000, () => {
-  console.log(`Started on port ${PORT}`);
+// app.listen(8000, () => {
+//   console.log(`Started on port ${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/index.html");
 });
 
+app.listen(8000, () => {
+  console.log("Server started on port 5000");
+});
