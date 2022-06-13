787d1d45245806d3035de9400356a14b2765e32a
munisht06 <munishtanwar77@gmail.com>
2022-06-12 23:01:21 -0400

    test

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-// const app = require("./index");
-// const PORT = process.env.PORT || 5000;
+const app = require("./index");
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Started on port ${PORT}`);
-// });
-
-const express = require("express");
-const app = express();
-
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
 });
+
