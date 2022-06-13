b2d3aed49a6b1aab78baf3026fc6c28eca24fbe9
munisht06 <munishtanwar77@gmail.com>
2022-06-13 15:10:57 -0400

    teset

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log("Server started on port 5000");
+  console.log(`"Server started on port 5000" ${process.env.PORT}`);
 });
